d37c6ecac60939aa0449288a437bca43dfd6098d
Tony Le <tonyhuyle123@gmail.com>
2024-03-28 19:36:40 -0500

    Added login error messages

--- dist/login.php
+++ dist/login.php
@@ -4,21 +4,23 @@ require(__DIR__ .'/../PhpFiles/loginModule.php');
 require(__DIR__ .'/../PhpFiles/loginValidation.php');
 use PhpFiles\userLogin; 
 use PhpFiles\loginValidation;
+$errors = array();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $validate = new loginValidation($_POST);
-  $errors = $validate->errors();
-  if(!empty($errors))
-  {
-    echo "Errors found";
-    exit();
-  }
-}
-
-if(!isset($_GET['error'])) { 
-    echo $_GET ['error']; 
-}
+if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $validate = new loginValidation($_POST);
+        $errors = $validate->is_valid();
+        if(empty($validate->errors()))
+        {
+            $module = new userLogin($_POST);
+        }
+        else
+        {
+            $errors = $validate->errors();
+        }
+    }
 ?>
+
 
 
 <!DOCTYPE html>
@@ -35,16 +37,37 @@ if(!isset($_GET['error'])) {
 
     <div class="max-w-md w-full p-6 bg-white rounded-md shadow-md">
         <h2 class="text-2xl font-semibold text-gray-800 mb-4">Client Portal</h2>
-      <form method = "post">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
           <label for="username" class="block text-sm text-gray-500">Username:</label>
           <input type="text" id="username" name="username" placeholder="Enter your username" class="mt-1 p-2 w-full border rounded-md">
       
+          <?php
+                    if($_SERVER["REQUEST_METHOD"] == "POST" and !empty($errors) and array_key_exists("username", $errors) )
+                    {
+                        $message = $errors["username"];
+                        echo "<Br><p style=\"color:red\"><strong>Error with input! Message: $message </strong></p>";
+                    }?>
+
           <label for="password" class="block mt-4 text-sm text-gray-500">Password:</label>
           <input type="password" id="password" name="password" placeholder="Enter your password" class="mt-1 p-2 w-full border rounded-md">
       
+          <?php
+                    if($_SERVER["REQUEST_METHOD"] == "POST" and !empty($errors) and array_key_exists("password", $errors) )
+                    {
+                        $message = $errors["password"];
+                        echo "<Br><p style=\"color:red\"><strong>Error with input! Message: $message </strong></p>";
+                    }?>
+
           <button class="mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
             Login
           </button>
+
+          <?php
+                    if($_SERVER["REQUEST_METHOD"] == "POST" and empty($errors))
+                    {
+                        echo "<Br><p><strong>Form submitted successfully! All input fields sucessfully validated.</strong></p>";
+                    }?>
+
       </form>
         <p class="mt-4 text-sm text-gray-500">
           Don't have an account? <a href="register.html" class="text-blue-500">Register here</a>.

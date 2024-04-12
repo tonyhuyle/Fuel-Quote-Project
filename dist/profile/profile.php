164608454a28948a89ca3d584c631e82412e051e
Nomar Ramos <ramos.nomar27@gmail.com>
2024-04-12 14:27:34 -0500

    fixed state select issue

--- dist/profile/profile.php
+++ dist/profile/profile.php
@@ -5,6 +5,8 @@
 	use PhpFiles\userProfile; 
 	use PhpFiles\profileValidation;
 
+    $states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+
 	//If the user is not logged in, redirect to the login page
 	if(!isset($_SESSION["CurrentUser"])){
         header("Location: ../login.php");
@@ -171,64 +173,12 @@
 
 				<div class="formGroup">
                     <label class="font-semibold" for="select">State</label><br>
-                    <select required
-                            name="state" 
-                            id="select" 
-                            minlength="2" 
-                            custommaxlength="2" 
-                            class="w-full p-2 border border-gray-300 rounded-md" 
-                            value="<?php echo $user->getState()?>" 
-                            >
-                        <option value="AL">AL</option>
-                        <option value="AK">AK</option>
-                        <option value="AZ">AZ</option>
-                        <option value="AR">AR</option>
-                        <option value="CA">CA</option>
-                        <option value="CO">CO</option>
-                        <option value="CT">CT</option>
-                        <option value="DE">DE</option>
-                        <option value="FL">FL</option>
-                        <option value="GA">GA</option>
-                        <option value="HI">HI</option>
-                        <option value="ID">ID</option>
-                        <option value="IL">IL</option>
-                        <option value="IN">IN</option>
-                        <option value="IA">IA</option>
-                        <option value="KS">KS</option>
-                        <option value="KY">KY</option>
-                        <option value="LA">LA</option>
-                        <option value="ME">ME</option>
-                        <option value="MD">MD</option>
-                        <option value="MA">MA</option>
-                        <option value="MI">MI</option>
-                        <option value="MN">MN</option>
-                        <option value="MS">MS</option>
-                        <option value="MO">MO</option>
-                        <option value="MT">MT</option>
-                        <option value="NE">NE</option>
-                        <option value="NV">NV</option>
-                        <option value="NH">NH</option>
-                        <option value="NJ">NJ</option>
-                        <option value="NM">NM</option>
-                        <option value="NY">NY</option>
-                        <option value="NC">NC</option>
-                        <option value="ND">ND</option>
-                        <option value="OH">OH</option>
-                        <option value="OK">OK</option>
-                        <option value="OR">OR</option>
-                        <option value="PA">PA</option>
-                        <option value="RI">RI</option>
-                        <option value="SC">SC</option>
-                        <option value="SD">SD</option>
-                        <option value="TN">TN</option>
-                        <option value="TX">TX</option>
-                        <option value="UT">UT</option>
-                        <option value="VT">VT</option>
-                        <option value="VA">VA</option>
-                        <option value="WA">WA</option>
-                        <option value="WV">WV</option>
-                        <option value="WI">WI</option>
-                        <option value="WY">WY</option>
+                    <select required name="state" id="select" minlength="2" custommaxlength="2" class="w-full p-2 border border-gray-300 rounded-md">
+                        <?php foreach ($states as $state): ?>
+                            <option value="<?php echo $state; ?>" <?php if ($state == $user->getState()) echo 'selected'; ?>>
+                                <?php echo $state; ?>
+                            </option>
+                        <?php endforeach; ?>
                     </select><br>
                     <span class="error font-semibold text-red-700"></span><br>
 	            </div>		

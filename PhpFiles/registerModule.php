725900a12a9cac39082c87e0fdf0f162f289f788
Tony Le <tonyhuyle123@gmail.com>
2024-04-12 20:03:09 -0500

    Register

--- PhpFiles/registerModule.php
+++ PhpFiles/registerModule.php
@@ -5,12 +5,6 @@ class userRegister {
     public $username = "JohnDoe123";
     public $password = "Password1!";
     public $confirmPassword = "Password1!";
-    protected $pdo;
-    protected function setUp(): void
-    {
-        require __DIR__ . '/../dist/connection.php';
-        $this->pdo = $pdo;
-    }
 
     public function getUsername(){
         return $this->username;
@@ -27,28 +21,5 @@ class userRegister {
     public function setPassword($password){
         $this->password = $password;
     }
-
-    public function register($username, $password) {
-        $this->setUp();
-            $pdo = $this->pdo;
-            
-        $passwordhash = password_hash($_POST["password"], PASSWORD_DEFAULT); // Hash the password
-
-        // Prepare SQL statement to insert user data
-        $query = $pdo->prepare("INSERT INTO users (username, passwordhash) VALUES (?, ?)");
-        $result = $query->execute([$username, $passwordhash]);
-        if($result) {
-            // Set the current user variable
-            $query = $pdo ->prepare("SELECT userid FROM users WHERE users.username = ? LIMIT 1");
-            $query ->execute([$username]);
-            $user = $query->fetch();
-            $_SESSION["CurrentUser"] = $user['userid'];
-            header("Location: ../dist/profile/profile.php");
-        } 
-        else {
-            $errors[] = "Registration failed. Please try again.";
-        }
 }
-}
-
 ?>
